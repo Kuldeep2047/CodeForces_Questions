c7c56795e57711035498007e1c2201a5be524e71
Kuldeep2047 <kuldeepgarg6993@gmail.com>
2025-11-21 22:20:52 +0530

    CF

---
+++ Rating_1100/Yarik_and_Array.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class Yarik_and_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                v[i] = sc.nextInt();
+            }
+
+            int ans = Integer.MIN_VALUE;
+            int i = 0, j = 0;
+            int sum = 0;
+
+            while (j < n) {
+                if (sum < 0) {
+                    sum = 0;
+                    i = j;
+                }
+
+                if (i < j) {
+                    if ((v[j] ^ v[j - 1]) % 2 != 0) {
+                        sum += v[j];
+                    } else {
+                        sum = v[j];
+                        i = j;
+                    }
+                } else {
+                    sum = v[j];
+                }
+
+                ans = Math.max(ans, sum);
+                j++;
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
